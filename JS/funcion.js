5f6dfc46768f4c4c19e4714c5696a8367bb88462
Ignacio Lopez <ignacioelopez04@gmail.com>
2023-02-09 15:14:52 -0300

    Responsive

---
+++ JS/funcion.js
@@ -0,0 +1,16 @@
+document.getElementById("btn-menu").addEventListener("click", mostrar_menu);
+document.getElementById("back_menu").addEventListener("click", ocultar_menu);
+
+nav = document.getElementById("nav");
+background_menu = document.getElementById("back_menu");
+
+function mostrar_menu(){
+    nav.style.right = "0px";
+    background_menu.style.display = "block";
+}
+
+function ocultar_menu(){
+    nav.style.right = "-275px";
+    background_menu.style.display = "none";
+}
+
