1f6ca9d5e29540abd7f095913b1b0144e085e2b0
Ignacio <ignacioelopez04@gmail.com>
2024-02-15 16:16:56 -0300

    Falta mejorar el menu cuando se hace click en los enlaces, pero en terminos generales todo funciona correctamente

--- cafe-app/src/Components/Li.jsx
+++ cafe-app/src/Components/Li.jsx
@@ -1,9 +1,9 @@
 import {Link} from 'react-scroll';
 
-export function Li({href, text, id = undefined}){
+export function Li({href, text, id = undefined, cerrarMenu}){
     return(
         <>
-            <li className="mr-4 text-lg border-b-[2px] hover:border-b-[1px] border-letra-color cursor-pointer respon-li">
+            <li className="mr-4 text-lg border-b-[2px] hover:border-b-[1px] border-letra-color cursor-pointer respon-li" onClick={cerrarMenu}>
                 <Link to={href} spy={true} smooth={true} offset={0} duration={500} id={id}>{text}</Link>
             </li> 
         </>
--- cafe-app/src/Components/NavMenu.jsx
+++ cafe-app/src/Components/NavMenu.jsx
@@ -1,17 +1,35 @@
 import { Li } from "./Li";
 import Logo from '../Assets/Logo.png';
 
-export function NavMenu(){
+export function NavMenu({abrirMenu, cerrarMenu}){
+
+    const navMenu = abrirMenu ? "bg-fondo-rojo top-0 right-[-275px] w-[500px] h-[200vh] p-[40px] z-1 transition-all duration-500 " : "main-nav";
+
     return(
-        <nav class ="main-nav" id="nav">
-            <img src={Logo} alt="Logo" className="hidden"></img>
-            <ul className="flex">
-                <Li href={"inicio"} id={"selected"} text={'Inicio'}></Li>
-                <Li href={"productos"} text={'Productos'}></Li>
-                {/* <Li href={"#ofertas"} text={'Ofertas'}></Li> */}
-                <Li href={"reservas"} text={'Reservas'}></Li>
-                <Li href={"contactos"} text={'Contactos'}></Li>
-            </ul>
-        </nav>
+        <>
+        <nav className={navMenu} id="nav">
+            {abrirMenu && 
+                <>
+                    <ul className='flex flex-col'>
+                        <Li href={"inicio"} id={"selected"} text={'Inicio'} cerrarMenu={cerrarMenu}></Li>
+                        <Li href={"productos"} text={'Productos'} cerrarMenu={cerrarMenu}></Li>
+                        {/* <Li href={"#ofertas"} text={'Ofertas'}></Li> */}
+                        <Li href={"reservas"} text={'Reservas'} cerrarMenu={cerrarMenu}></Li>
+                        <Li href={"contactos"} text={'Contactos'} cerrarMenu={cerrarMenu}></Li>
+                    </ul>
+                </>
+                ||
+                <>    
+                    <ul className='flex'>
+                        <Li href={"inicio"} id={"selected"} text={'Inicio'}></Li>
+                        <Li href={"productos"} text={'Productos'}></Li>
+                        {/* <Li href={"#ofertas"} text={'Ofertas'}></Li> */}
+                        <Li href={"reservas"} text={'Reservas'}></Li>
+                        <Li href={"contactos"} text={'Contactos'}></Li>
+                    </ul>
+                </>
+            }
+            </nav>
+        </>
     );
 }
